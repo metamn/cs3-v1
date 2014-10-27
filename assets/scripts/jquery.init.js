c732f905f84d3e91c0758b04c1889cce340f6b52
cs <bartus.csongor@gmail.com>
2014-10-27 18:59:22 +0200

    Breakpoints done, first iteration

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
   
   // Skills
-  $('.frontpage article.clickable').click(function() {
-    $(this).find('aside').slideToggle('slow');
-  });
-  
-  // CMS
-  $('.frontpage #cms article h3').click(function() {
-    $(this).next('aside').slideToggle('slow');
+  $('.accordion').click(function() {
+    $(this).toggleClass('active');
   });
 });
 
